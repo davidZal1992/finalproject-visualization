5900b81d31be4453f01a43779db58b45a58ac254
David <davidzal@post.bgu.ac.il>
2020-12-30 23:50:46 +0200

    create update data forms from client

--- server/routers/actions.py
+++ server/routers/actions.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter,Header,HTTPException
+from typing import List
+from fastapi import APIRouter,Header,HTTPException,FastAPI, File, UploadFile
+
 router = APIRouter()
 
+import pandas as pd
 
-@router.get("/")
-async def generate_process():
-    return [{"Test": "Test"}, {"Test": "Test"}]
+@router.post('/upload')
+def upload_file(files: List[UploadFile] = File(...)):
+    miRNA_data = pd.read_csv(files[0].file)
+    targets_data = pd.read_csv(files[1].file)
+    connections_data = pd.read_csv(files[2].file)
+
+    
