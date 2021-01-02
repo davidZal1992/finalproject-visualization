33061c503c7a1d14b9092da9d495d7976957735c
David <davidzal@post.bgu.ac.il>
2021-01-02 16:48:31 +0200

    update homepage with basic settings

---
+++ client/components/index.js
@@ -0,0 +1,8 @@
+//Primary index.js file
+
+$(document).ready(() =>{
+    console.log('ad')
+    var table = document.getElementById("table-box");
+    table.style.display="none";
+})
+  
--- client/components/uplaodfiles.js
+++ client/components/uplaodfiles.js
@@ -9,7 +9,7 @@ function generate() {
   formData.append("files", document.getElementById('mirNA').files[0]);
   formData.append("files", document.getElementById('target').files[0]);
   formData.append("files", document.getElementById('connection').files[0]);
-  console.log('check')
+
   axios.post('http://127.0.0.1:8000/actions/upload', formData, {
     headers: {
       'content-Type': 'multipart/form-data',
