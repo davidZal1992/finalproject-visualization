acdeb6ee7d8151959af3ea673d19163d6cefc8f1
galborabia <49067563+galborabia@users.noreply.github.com>
2021-03-16 21:35:39 +0200

    deseq merge

--- client/components/dseq-modal.js
+++ client/components/dseq-modal.js
@@ -1,32 +1,34 @@
 const axios = require('axios')
+const plotly = require ("plotly.js-dist");
+const { Parser } = require('json2csv');
+
 
 var hiddenInput=true;
+
 document.getElementById('href-instructions').addEventListener('click', navgiateToInstructions);
 document.getElementById('href-parameters').addEventListener('click', navgiateToParameters);
 
 document.getElementById("dseq-user-file").addEventListener('click', function(e){
     $("#user-analysis-input").attr('hidden',!hiddenInput);
     hiddenInput=!hiddenInput
-
 });
+
 document.getElementById('dseq_files').addEventListener('click', function(e) { 
     uploadDseqFiles(e)},false);
 
 document.getElementById('dseq_analysys').addEventListener('click',function(e) {
     runAnalysis(e)},false);
 
-document.getElementById('dseq_plot').addEventListener('click',function(e) {
+document.getElementById('dseq_analysys-download').addEventListener('click',function(e) {
+    download_deseq_result(e)},false);
+
+document.getElementById('deseq_plot').addEventListener('click',function(e) {
     plotDseq(e)},false);
     
-    
-
-
-
 function navgiateToInstructions(){
     document.getElementById("deseq_instructions").style.display="block";
     document.getElementById("deseq_parameters").style.display="none";
 }
-
 
 function navgiateToParameters(){
     document.getElementById("deseq_instructions").style.display="none";
@@ -34,6 +36,7 @@ function navgiateToParameters(){
 }
 
 function uploadDseqFiles(e){
+        e.preventDefault();
         $('#green').remove();
 
         files =["count_matrix_data_input","design_matrix_input"]
@@ -59,8 +62,6 @@ function uploadDseqFiles(e){
                 designMatrixDropDown = document.getElementById('design-matrix-select');
                 addValues(response.data.design_values,designMatrixDropDown)
                 $( "#dseq_analysys" ).prop( "disabled", false );
-        
-
           })
           .catch(error => {
             initAnaylsysValues()  
@@ -74,14 +75,14 @@ function uploadDseqFiles(e){
           })
         }
 
-        function addValues(valuesArray,selectElem){
-            valuesArray.sort()
-            for(var i = 0; i<valuesArray.length; i++){
-                selectElem.options[i] = new Option(valuesArray[i],valuesArray[i])
-            }
-        }
+function addValues(valuesArray,selectElem){
+    valuesArray.sort()
+    for(var i = 0; i<valuesArray.length; i++){
+        selectElem.options[i] = new Option(valuesArray[i],valuesArray[i])
+    }
+}
 
-function  runAnalysis(e){
+function runAnalysis(e){
     let formData = new FormData();
     e.preventDefault();
     count_matrix_id = document.getElementById("count-matrix-select").value
@@ -113,8 +114,47 @@ function  runAnalysis(e){
       })
 }
 
+function download_deseq_result(e){
+    e.preventDefault();
+    console.log("download data");
+    axios.get('http://127.0.0.1:8000/deseq/get_deseq_result',{
+        headers: {
+          'content-Type': 'multipart/form-data',
+          "Access-Control-Allow-Origin": "*"
+        }
+        }).then((response) => {
+          console.log(response.data); 
+          csv_data = json2csv(response.data);
+          const csvBlob = new Blob([csv_data],{type: "text/csv"});
+          const blobUrl = URL.createObjectURL(csvBlob);
+          const anchorElement = document.createElement("a");
+          anchorElement.href = blobUrl;
+          anchorElement.download = "deseq_result.csv"
+          anchorElement.click();
+          setTimeout(() => {
+            URL.revokeObjectURL(blobUrl);
+          },600)
+      })
+      .catch(error => {
+        var para = document.createElement("p");            
+            para.innerText = "*Some Error occured. Check your files";
+            para.style.color="red"
+            document.getElementById("dseqAnalysForm").appendChild(para);
+            setTimeout(() => {
+                para.remove();
+            },6000)
+      })
+}
 
-function plotDseq(){
+function json2csv(data){
+    json_data = JSON.parse(data);
+    const json2csvparser = new Parser();
+    const csv = json2csvparser.parse(json_data)
+    return csv
+}
+
+function plotDseq(e){
+    e.preventDefault();
     let formData = new FormData();
     if(!hiddenInput){
         formData.append('files', document.getElementById("dseq-anlysis-input").files[0]);
@@ -153,12 +193,25 @@ function plotDseq(){
       })
     }
 
-
 function initAnaylsysValues(){
     $('#green2').remove();
     $("#dseq_analysys").prop("disabled",true)
     $("#dseq_analysys-download").attr('hidden',true);
     $("#count-matrix-select option").remove();
     $("#design-matrix-select option").remove();
+}
 
+function create_volcano_plot(html){
+  volcano_plot_data =html['data'];
+  // var icon = 
+  var config = {
+    modeBarButtonsToAdd: [
+      {
+        name: 'download data',
+        // icon: 'download_icon.png',
+        click: function() {
+          console.log(volcano_plot_data);
+        }}
+    ]};
+  plotly.newPlot('plot',html['data'],html['layout'],config)
 }
--- server/routers/deseq_router.py
+++ server/routers/deseq_router.py
@@ -6,20 +6,28 @@ import pandas as pd
 from typing import List
 import shutil
 from controller import deseq_controller
+from routers.Unicorn_Exception import UnicornException
 
 router = APIRouter()
 
 @router.post('/run_deseq')
 async def run_deseq(request :Request):
-    # data = await request.form()
-    # deseq_result =  deseq_controller.run_deseq_controller(data)
-    # return {'deseq_results':deseq_result}
-    return "bla"
+    data = await request.form()
+    # uuid = request.headers['uuid']
+    # JUST FOR TEST TO AVOID A LOT OF FILES
+    uuid = 'aae10d89-5fed-4fb4-b2d7-1ac709fb9534'
+    deseq_result =  deseq_controller.run_deseq_controller(data,uuid)
+    return {'deseq_results':deseq_result}
+
 
 @router.post('/volcano_plot_deseq',response_class=HTMLResponse)
 async def deseq_volcano(request :Request):
     data = await request.form()
-    fig_json = deseq_controller.deseq_volcano_controller(data)
+    # uuid = request.headers['uuid']
+    # JUST FOR TEST TO AVOID A LOT OF FILES
+    #
+    uuid = 'aae10d89-5fed-4fb4-b2d7-1ac709fb9534'
+    fig_json = deseq_controller.deseq_volcano_controller(data,uuid)
     return fig_json
 
 @router.post('/upload_data')
@@ -28,20 +36,16 @@ async def upload_data(files:List = File(...)):
         # JUST FOR TEST TO AVOID A LOT OF FILES
         #
         uuid='aae10d89-5fed-4fb4-b2d7-1ac709fb9534'
-
         locations=[f"upload_data/{uuid}/count_matrix.csv",f"upload_data/{uuid}/design_matrix.csv"]
-
-        print('aaa')
         i=0
         for file in files:
             if file.filename.endswith('.csv'):
-                file_name = file.filename
                 with open(locations[i], "wb+") as file_object:
                     file_object.write(file.file.read())
                     file_object.close()
-
             else:
-                raise Exception("Csv files only")
+                raise UnicornException(name="Only csv files", status_code=404,
+                                       details="User can only upload csv files")
             i+=1
         count_values = get_ids("count_matrix.csv",uuid)
         design_values = get_ids("design_matrix.csv",uuid)
@@ -51,6 +55,17 @@ def get_ids(filename,uuid):
     data = pd.read_csv(f"upload_data/{uuid}/{filename}")
     names = data.columns.tolist()
     return names
+
+@router.get('/get_deseq_result')
+async def get_files_names(request: Request):
+    # uuid = request.headers['uuid']
+    # JUST FOR TEST TO AVOID A LOT OF FILES
+    #
+    uuid = 'aae10d89-5fed-4fb4-b2d7-1ac709fb9534'
+    deseq_result = pd.read_csv(f"upload_data/{uuid}/deseq_result.csv")
+    json_data = deseq_result.to_json(orient="records")
+    return json_data
+
 
 
 # @router.get('/get_files')
